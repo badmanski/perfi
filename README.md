9d1b247073e99a708aedcdf47f8c14efc36965db
badmanski <stanislav.gorski@gmail.com>
2013-11-23 14:58:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Finance planner web app
+
+### Tech specs
+* Ruby 2.0.0
+* Rails 4.0.0
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Finance::Application.config.secret_key_base = '9027a3ae969accd535166d51d3df9b757b2ae5a39e90362f2696ec132ea169c889cac8e22ed084b51893ca32ad20d9d1d73ca6b3512235d113afc82e9f7c3180'
