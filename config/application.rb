e90e25c6ea95f67bf00608dda18181b492640e5d
badmanski <stanislav.gorski@gmail.com>
2013-11-30 20:22:02 +0200

    Add assets for precompile

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Finance
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.precompile += ['login.css']
+
     config.generators do |g|
       g.fixture_replacement :machinist
       g.helper false
